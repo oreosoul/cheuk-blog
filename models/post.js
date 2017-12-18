164c04767308398c8e7cb3ccc5d7d5f86d1eaea2
cheuk.li <cheuk.li@bamboonetworks.com>
2017-12-18 18:40:34 +0800

    标签功能

--- models/post.js
+++ models/post.js
@@ -1,9 +1,10 @@
 var mongodb = require('./db'),
     markdown = require('markdown').markdown
 
-function Post(name, title, post){
+function Post(name, title, tags, post){
     this.name = name
     this.title = title
+    this.tags = tags
     this.post = post
 }
 
@@ -26,6 +27,7 @@ Post.prototype.save = function(callback){
         name: this.name,
         time: time,
         title: this.title,
+        tags: this.tags,
         post: this.post,
         comments: []
     }
@@ -205,6 +207,7 @@ Post.remove = function(name, minute, title, callback){
     })
 }
 
+//获取全部文章存档
 Post.getArchive = function(callback){
     //返回文章存档信息
     mongodb.open(function(err, db){
@@ -224,10 +227,57 @@ Post.getArchive = function(callback){
                 time: -1
             }).toArray(function(err, docs){
                 mongodb.close()
-                if(err){
-                    return callback(err)
-                }
+                if(err) return callback(err)
+                
                 callback(null, docs)
+            })
+        })
+    })
+}
+
+//获取全部标签
+Post.getTags = function(callback){
+    mongodb.open(function(err, db){
+        if(err) return callback(err)
+
+        db.collection('post', function(err, collection){
+            if(err){
+                mongodb.close()
+                return callback(err)
+            }
+            collection.distinct('tags', function(err, docs){
+                mongodb.close()
+                if(err) return callback(err)
+
+                callback(null, docs)
+            })
+        })
+    })
+}
+
+Post.getTag = function(tag, callback){
+    mongodb.open(function(err, db){
+        if(err) return callback(err)
+
+        db.collection('post', function(err, collection){
+            if(err){
+                mongodb.close()
+                return callback(err)
+            }
+            collection.find({
+                "tags": tag
+            },{
+                "name": 1,
+                "time": 1,
+                "title": 1
+            }).sort({
+                "time": -1
+            }).toArray(function (err, docs) {
+                mongodb.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null, docs);
             })
         })
     })
--- routes/index.js
+++ routes/index.js
@@ -118,7 +118,8 @@ module.exports = function (app) {
     app.post('/post', checkLogin)
     app.post('/post', function (req, res) {
         var currentUser = req.session.user,
-            post = new Post(currentUser.name, req.body.title, req.body.post)
+            tags = [req.body.tag1, req.body.tag2, req.body.tag3],
+            post = new Post(currentUser.name, req.body.title, tags, req.body.post)
         post.save(function(err){
             if(err){
                 req.flash('error', err)
@@ -272,6 +273,39 @@ module.exports = function (app) {
             }
             res.render('archive', {
                 title: '存档',
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            })
+        })
+    })
+
+    //获取全部标签页面
+    app.get('/tags',function(req, res){
+        Post.getTags(function(err, tags){
+            if(err){
+                req.flash('error', err)
+                return res.redirect('/')
+            }
+            res.render('tags', {
+                title: '标签',
+                tags: tags,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            })
+        })
+    })
+    //获取指定标签页面
+    app.get('/tags/:tag',function(req, res){
+        Post.getTag(req.params.tag, function(err, posts){
+            if(err){
+                req.flash('error', err)
+                return res.redirect('/')
+            }
+            res.render('tag', {
+                title: 'tag' + req.params.tag,
                 posts: posts,
                 user: req.session.user,
                 success: req.flash('success').toString(),
