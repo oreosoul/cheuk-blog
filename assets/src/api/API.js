7fe01a9454bdefe70de6aa2c1d11a188666ec076
Cheuk <654181494@qq.com>
2018-01-17 23:01:36 +0800

    前端主页

--- assets/src/api/API.js
+++ assets/src/api/API.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
-// import config from './config'
-// import qs from 'qs'
+
 axios.defaults.baseURL = 'http://localhost:3000'
 class Api {
   getPostList () {
--- assets/src/api/config.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios'
-
-axios.defaults.baseURL = 'localhost:3000'
